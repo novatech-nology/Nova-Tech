422efcaa2333ff7ed7e80a38c5da8125e289fa8b
yVini2k <vinicius.besilva.dev@gmail.com>
2026-06-12 20:20:45 -0300

    Configura deploy no Railway

---
+++ railway/start.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+
+php artisan config:clear
+php artisan route:clear
+php artisan view:clear
+
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+php artisan serve --host=0.0.0.0 --port=${PORT:-8080}
